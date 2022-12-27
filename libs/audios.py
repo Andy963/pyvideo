47643bf544d3d34af23b1e7a3906764e5ff348b1
Andy963 <Andy963@users.noreply.github.com>
2022-12-27 15:17:21 +0800

    update:random choice mask, add bgm etc.

--- libs/audios.py
+++ libs/audios.py
@@ -3,12 +3,13 @@
 import os
 
 from moviepy.audio.AudioClip import CompositeAudioClip, concatenate_audioclips
+from moviepy.audio.fx.all import audio_loop
 from moviepy.audio.io.AudioFileClip import AudioFileClip
 from moviepy.video.VideoClip import ImageClip
 from moviepy.video.io.VideoFileClip import VideoFileClip
 
 from libs import LOG, timer, CPU_COUNT, cal_sec
-from libs.files import save_filelist
+from libs.files import save_filelist, random_mask
 
 
 def transcode_audio(src_path, dst_ext='mp3'):
@@ -44,18 +45,26 @@ def combine_audios(src_path, out_path):
     合并音频
     """
     files = save_filelist(src_path)
-    try:
-        cmd = f'ffmpeg -f concat -safe 0 -i list.txt -c copy {out_path}'
-        os.system(cmd)
-    except Exception as e:
-        av = []
-        for a in files:
-            av.append(AudioFileClip(a))
-        fvs = concatenate_audioclips(av)
-        fvs.write_audiofile(out_path, logger=LOG)
+    # try:
+    #     cmd = f'ffmpeg -f concat -safe 0 -i list.txt -c copy {out_path}'
+    #     os.system(cmd)
+    # except Exception as e:
+    av = []
+    for a in files:
+        av.append(AudioFileClip(a))
+    fvs = concatenate_audioclips(av)
+    fvs.write_audiofile(out_path, logger=LOG)
 
 
-def add_bg_ms(v_path, ms_path, out_path, v_start=0, v_end=None, ms_start=0, ms_end=None, v_has_ms=False):
+def add_bgm(
+        v_path, ms_path,
+        out_path=None,
+        v_start=0,
+        v_end=None,
+        ms_start=0,
+        ms_end=None,
+        v_has_ms=False,
+        loop=True):
     """
     给视频添加背景音乐
     :param v_path: 视频路径
@@ -66,35 +75,47 @@ def add_bg_ms(v_path, ms_path, out_path, v_start=0, v_end=None, ms_start=0, ms_e
     :param ms_start: 背景音乐开始时间
     :param ms_end: 背景音乐结束时间
     :param v_has_ms: 视频是否有背景音乐
+    :param loop: 是否循环背景音乐
     """
     video = VideoFileClip(v_path)
+    for i in [v_start, v_end, ms_start, ms_end]:
+        if isinstance(i, str):
+            i = cal_sec(i)
     if v_start:
         video = video.subclip(v_start, video.duration)
     if v_end:
         video = video.subclip(0, v_end)
-    bg_ms = AudioFileClip(ms_path)
+    bgm = AudioFileClip(ms_path)
     if ms_start:
-        bg_ms = bg_ms.subclip(ms_start, bg_ms.duration)
+        bgm = bgm.subclip(ms_start, bgm.duration)
     if ms_end:
-        bg_ms = bg_ms.subclip(0, ms_end)
+        bgm = bgm.subclip(0, ms_end)
     # 视频本身无背景音乐，直接设置背景音乐
+    if loop:
+        bgm = audio_loop(bgm, duration=video.duration)
     if not v_has_ms:
-        video = video.set_audio(bg_ms)
+        video = video.set_audio(bgm)
     # 视频本身是有音乐背景的，则合并两者
     else:
-        com_audio = CompositeAudioClip([video.audio, bg_ms])
+        com_audio = CompositeAudioClip([video.audio, bgm])
         video.set_audio(com_audio)
+    if not out_path:
+        name, ext = os.path.splitext(v_path)
+        out_path = f"{name}_bgm{ext}"
     video.write_videofile(out_path)
+    video.close()
 
 
 @timer
-def add_static_image_to_audio(image_path, audio_path, output_path):
+def add_static_image_to_audio(audio_path, output_path, image_path=None, ffmpeg_params=None):
     """Create and save a video file to `output_path` after
     combining a static image that is located in `image_path`
     with an audio file in `audio_path`"""
     # create the audio clip object
     audio_clip = AudioFileClip(audio_path)
     # create the image clip object
+    if not image_path:
+        image_path = random_mask()
     image_clip = ImageClip(image_path)
     # use set_audio method from image clip to combine the audio with the image
     video_clip = image_clip.set_audio(audio_clip)
@@ -103,7 +124,8 @@ def add_static_image_to_audio(image_path, audio_path, output_path):
     # set the FPS to 1
     video_clip.fps = 1
     # write the resuling video clip
-    video_clip.write_videofile(output_path, preset='ultrafast', threads=CPU_COUNT, logger=LOG)
+    video_clip.write_videofile(output_path, preset='ultrafast', threads=CPU_COUNT, logger=LOG,
+                               ffmpeg_params=ffmpeg_params)
     video_clip.close()
 
 
@@ -112,7 +134,7 @@ def split_audio(src_path, dst_path, start=None, end=None):
     分割音频
     """
     try:
-        cmd = f'ffmpeg -i {src_path} -acodec copy -ss {start} -to {end} {dst_path}'
+        cmd = f'ffmpeg -i "{src_path}" -acodec copy -ss {start} -to {end} {dst_path}'
         os.system(cmd)
     except Exception as e:
         print(e)
--- libs/files.py
+++ libs/files.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python
 # coding:utf-8
 import os
+import random
 
 
 def list_file(src_path='audio'):
     """列出所有的文件"""
     fs = []
-    print(f"开始遍历文件：")
     for file in os.listdir(src_path):
         fs.append(os.path.join(src_path, file))
-    print(f"共找到{len(fs)}个文件.顺序为：")
-    for i, f in enumerate(fs, start=1):
-        print(f"{i}. {f}")
+    print(f"共找到{len(fs)}个文件")
     return fs
 
 
@@ -25,3 +23,11 @@ def save_filelist(src_path):
     with open(f_name, 'w', encoding='utf-8') as f:
         f.write(data)
     return files
+
+
+def random_mask(face_path='mask'):
+    """随机选择一张封面"""
+    files = list_file(face_path)
+    rs = random.choice(files)
+    print('随机选择的封面为：', rs)
+    return rs
